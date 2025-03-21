df8e52521f99a3794d4a68d54a64e2531ffe8db6
tildesarecool <tildesare.cool@gmail.com>
2025-03-20 21:00:25 -0700

    fixed a typo in the tester

--- Tests/gamelibautoarchiver.Tests.ps1
+++ Tests/gamelibautoarchiver.Tests.ps1
@@ -1,5 +1,6 @@
+# Tests/gamelibautoarchiver.Tests.ps1
 Describe "GameLibAutoArchiver Module" {
     It "Module should exist" {
-        Test-Path "$PSScriptRoot\..\GameLibAutoArchiver.psm1" | Should -Be $true
+        Test-Path "$PSScriptRoot\..\GameLibAutoArchiver\gamelibautoarchiver.psm1" | Should -Be $true
     }
 }
